9e052946694b056b9ac7af7ec4fb524eb4a98c0d
Soumya Kanta Sahoo <iamsoumyakantasahoo@gmail.com>
2026-06-09 09:09:26 +0530

    updating

--- PYTHON/Practices/osmodule.py
+++ PYTHON/Practices/osmodule.py
@@ -1,80 +1,82 @@
-# 1. Automatic Daily Supplier Ingestion Folder
+# # 1. Automatic Daily Supplier Ingestion Folder
 
 import os
 
-def initiate_business_vault(folder_name):
+current_dir = os.getcwd()
 
-    if os.path.exists(folder_name):
-        print(f"\n{folder_name} already exists, bypass encryption.\n")
-    else:
-        os.mkdir(folder_name)
-        print(f"\n{folder_name} Created Successfully!.\n")
+# def initiate_business_vault(folder_name):
 
-initiate_business_vault("folder_name")
+#     if os.path.exists(folder_name):
+#         print(f"\n{folder_name} already exists, bypass encryption.\n")
+#     else:
+#         os.mkdir(folder_name)
+#         print(f"\n{folder_name} Created Successfully!.\n")
 
-
-# 2. The Safe Path Log Ingestor
-
-def secure_file_dump(directory, filename, data_string):
-
-    perfect_path = os.path.join(directory, filename)
-
-    os.makedirs(directory, exist_ok=True)
-
-    with open(perfect_path, "a") as f:
-        f.write(f"{data_string}\n")
-
-secure_file_dump("Khushboo_Agency_Vault", "bills.txt", "Amul High Protein Buttermilk - 200 Packs Received")
+# initiate_business_vault("folder_name")
 
 
-# 3. Live Directory Inspector (Audit System)
+# # 2. The Safe Path Log Ingestor
 
-def audit_storage_folder(folder_path):
+# def secure_file_dump(directory, filename, data_string):
 
-    files_list = os.listdir(folder_path)
+#     perfect_path = os.path.join(directory, filename)
 
-    with open("files_list.txt", "a") as f:
-        f.write(f"Files List in {folder_path} are :- \n")
+#     os.makedirs(directory, exist_ok=True)
 
-        for file in files_list:
-            print(f"{file}")
-            f.write(f"{file}\n")
+#     with open(perfect_path, "a") as f:
+#         f.write(f"{data_string}\n")
 
-audit_storage_folder("PYTHON")
+# secure_file_dump("Khushboo_Agency_Vault", "bills.txt", "Amul High Protein Buttermilk - 200 Packs Received")
 
 
-# 4. The Folder Auditor (Files Scan Matrix)
+# # 3. Live Directory Inspector (Audit System)
 
-def audit_my_warehouse(folder_name):
+# def audit_storage_folder(folder_path):
+
+#     files_list = os.listdir(folder_path)
+
+#     with open("files_list.txt", "a") as f:
+#         f.write(f"Files List in {folder_path} are :- \n")
+
+#         for file in files_list:
+#             print(f"{file}")
+#             f.write(f"{file}\n")
+
+# audit_storage_folder("PYTHON")
+
+
+# # 4. The Folder Auditor (Files Scan Matrix)
+
+# def audit_my_warehouse(folder_name):
     
-    if not os.path.exists(folder_name):
-        print(f"❌ Audit Cancelled: Target Folder doesn't exist.")
-    else:
-        lists = os.listdir(folder_name)
-        with open("files_list.txt", "a") as f:
-            f.write(f"List of files in {folder_name }are :- ")
+#     if not os.path.exists(folder_name):
+#         print(f"❌ Audit Cancelled: Target Folder doesn't exist.")
+#     else:
+#         lists = os.listdir(folder_name)
+#         with open("files_list.txt", "a") as f:
+#             f.write(f"List of files in {folder_name }are :- ")
 
-            for i in lists:
-                print(f"\n{i}")
-                f.write(f"{i}")
+#             for i in lists:
+#                 print(f"\n{i}")
+#                 f.write(f"{i}")
 
-audit_my_warehouse("PYTHON")
+# audit_my_warehouse("PYTHON")
 
 
 
-# 5. The Bulk Ledger Backup System
+# # 5. The Bulk Ledger Backup System
 
-def bulk_ledger_backup(vault_name, file_list):
+# def bulk_ledger_backup(vault_name, file_list):
 
-    os.makedirs(vault_name, exist_ok=True)
+#     os.makedirs(vault_name, exist_ok=True)
 
-    for item in file_list:
-        perfect_path = os.path.join(vault_name, item)
+#     for item in file_list:
+#         perfect_path = os.path.join(vault_name, item)
 
-        with open(perfect_path, "w") as f:
-            f.write(f'Backup Logs Secured\n')
+#         with open(perfect_path, "w") as f:
+#             f.write(f'Backup Logs Secured\n')
 
-bulk_ledger_backup("vault_name", ["morning.txt", "evening.txt"])
+# bulk_ledger_backup("vault_name", ["morning.txt", "evening.txt"])
 
 
 # 6. The Intelligent Janitor (Old Logs Deleter)
@@ -93,6 +95,18 @@ def system_cleaner(folder_name, file_to_delete):
 system_cleaner("vault_name", "morning.txt")
 system_cleaner("vault_name", "evening.txt")
 system_cleaner("PYTHON", "june_cash_ledger.txt")
+system_cleaner("Khushboo_Agency_Vault", "bills.txt")
+system_cleaner(current_dir, "files_list.txt")
+system_cleaner(current_dir, "approved_suppliers.txt")
+system_cleaner(current_dir, "business_directory.txt")
+system_cleaner(current_dir, "cash_ledger.txt")
+system_cleaner(current_dir, "daily_expenses.txt")
+system_cleaner(current_dir, "inventory_log.txt")
+system_cleaner(current_dir, "security_key.txt")
+system_cleaner(current_dir, "session_log.txt")
+system_cleaner(current_dir, "system_logs.txt")
+system_cleaner(current_dir, "supplier_ledger.txt")
+
 
 
 # 7. Folder termination permanently
@@ -101,12 +115,11 @@ import shutil
 
 def folder_terminator(sub_folder):
 
-    folder_name = os.getcwd()
 
-    perfectpath = os.path.join(folder_name, sub_folder)
+    perfectpath = os.path.join(current_dir, sub_folder)
 
     if os.path.exists(perfectpath):
-        os.removedirs(perfectpath)
+        os.rmdir(perfectpath)
         print(f"Path termination successfully")
     else:
         print(f'⚠️ Path not found. Clean operation bypassed.')
